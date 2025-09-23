800a7687d0a44f53d2e2a9da76ff0dd77739ab28
Murali0314 <gorlamuralidhar@gmail.com>
2025-09-23 23:06:53 +0530

    Update api.js

--- frontend/ams-frontend/src/api.js
+++ frontend/ams-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://attendance-management-system-17.onrender.com' || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
